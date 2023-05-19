eb09ca8e2a8266a5d7ead2626653b14ae09ba406
yago <yagom10@hotmail.com>
2023-05-18 19:23:12 -0700

    updating

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("running scripts")
-
-fetch("https://www.aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=KDE")
-.then(response => response.json())
-.then(data => console.log(data))
