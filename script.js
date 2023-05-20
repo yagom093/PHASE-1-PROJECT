5fe06f1e874c3313293fb7be8c09aa7be14bad87
yago <yagom10@hotmail.com>
2023-05-20 02:27:33 -0700

    updating

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('javascript connected')
+
+
+function getWeather(city) {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={94f64ef5fab0b721c754f9bf1fbb8703")
+      .then(response => response.json())
+      .then(data => {
+        if (data.cod === '404') {
+          showError(data.message);
+        } else {
+          showWeather(data);
+        }
+      })
+      .catch(error => {
+        console.log('Error:', error);
+        showError('Failed to retrieve weather data. Please try again later.');
+      });
+  }
